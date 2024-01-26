423bce72246638be11f420fd79829c183ac150bf
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-01-26 09:54:25 +0200

    js-html rakenne on valmis

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ otsikko.appendChild(node1);
 ylatunniste.appendChild(otsikko);
 // 1.2 headerin kappale
 const kappale1 = document.createElement("p");
-const node2 = document.createTextNode("Ensimmäinen kappale Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam quaerat nemo dolorum nostrum reiciendis odio.");
+const node2 = document.createTextNode("Ensimmäinen kappale Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam quaerat nemo dolorum nostrum reiciendis odio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam quaerat nemo dolorum nostrum reiciendis odio.");
 kappale1.appendChild(node2);
 // 1.3 lisätään headerin lapset
 ylatunniste.appendChild(kappale1);
@@ -19,7 +19,8 @@ osa.setAttribute("id", "rinnakkain");
 osa.style.display = "flex";
 //2.1 Sectionin kappale
 const kappale2 = document.createElement("p");
-const node3 = document.createTextNode("Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam quaerat nemo dolorum nostrum reiciendis odio.");
+const node3 = document.createTextNode("Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam quaerat nemo dolorum nostrum reiciendis odio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam. Ensimmäinen kappale Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam quaerat nemo dolorum nostrum reiciendis odio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aperiam iste necessitatibus earum ipsam quaerat nemo dolorum nostrum reiciendis odio.");
+kappale1.appendChild(node2);
 kappale2.appendChild(node3);
 // 2.2 Sectionin kuva
 const raami = document.createElement("figure");
@@ -37,7 +38,7 @@ raami.style.flex = "1";
 const alatunniste = document.createElement("footer");
 // 3.1 footerin kappale
 const kappale3 = document.createElement("p")
-const node4 = document.createTextNode("copiright 2024 Israt Jahan Naishe")
+const node4 = document.createTextNode("@copiright 2024 Israt Jahan Naishe")
 kappale3.appendChild(node4);
 // 3.2 lisätään footerin lapsi
 alatunniste.appendChild(kappale3)
